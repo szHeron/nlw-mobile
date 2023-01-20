ddb420e1d72891c8c7027bd41cdb0c0b9d72a899
szheron <heronbr7@gmail.com>
2023-01-20 09:47:30 -0300

    adicionado a tabela de habitos

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Touchable, TouchableOpacity, View, Text } from "react-native";
+import Feather from '@expo/vector-icons/Feather';
+import colors from 'tailwindcss/colors'
+import Logo from '../../assets/logo.svg'
+
+export function Header(){
+    return (
+        <View className="w-full flex-row items-center justify-between">
+            <Logo/>
+            <TouchableOpacity className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
+                <Feather
+                    name="plus"
+                    size={20}
+                    color={colors.violet[500]}
+                />
+                <Text className="text-white ml-3 font-semibold text-base">
+                    Novo
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
